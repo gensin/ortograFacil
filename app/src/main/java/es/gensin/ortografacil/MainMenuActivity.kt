b6d16765e4b720d8ac6b05b3e990df8161f481fe
Pau Arlandis Martinez <ghostdesert@gmail.com>
2018-05-16 01:29:32 +0200

    Initial commit

---
+++ app/src/main/java/es/gensin/ortografacil/MainMenuActivity.kt
@@ -0,0 +1,12 @@
+package es.gensin.ortografacil
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
